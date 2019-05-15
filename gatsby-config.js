16c0c1645971ecf10984b46dd614ab4ba6cb7993
GBFG Guide <gbfg.guide@gmail.com>
2019-05-14 22:27:56 -0400

    add gatsby plugins, configure plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,19 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-prismjs`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
